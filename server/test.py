2588e90c87f96bac2341f92c9f8fd902a61209e3
millercc23 <91136427+millercc23@users.noreply.github.com>
2023-04-18 11:19:04 -0400

    added test to test  multiple img, model1_inc is trained on expaned training set

--- server/test.py
+++ server/test.py
@@ -2,31 +2,40 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
-
+import pathlib
 
 
 # Recreate the exact same model, including its weights and the optimizer
-new_model = tf.keras.models.load_model('/home/cole/github/gun-not-gun/models/model1.h5')
+new_model = tf.keras.models.load_model('C:/Users/cammi/OneDrive/Documents/GitHub/gun-not-gun/models/model1_inc.h5')
 
 # Show the model architecture
 new_model.summary()
 
 # Load and preprocess a single image
-img_path = '/home/cole/Desktop/random.jpg'
-img = image.load_img(img_path, target_size=(112, 112)) # resize to match model input size
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0) # add batch dimension
-img_array = img_array / 255.0 # normalize pixel values
+img_path = 'C:/Users/cammi/Downloads/gunModel_Tester'
+data_dir = pathlib.Path(img_path)
+img_list = list(data_dir.glob('*/*.jpg'))
+print(len(img_list))
+print('lllllllllllll')
+i = 0
+img_count = {'gun': 0, 'no gun': 0}
+while i < len(img_list):
+    img = image.load_img(img_list[i], target_size=(112, 112)) # resize to match model input size
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0) # add batch dimension
+    #img_array = img_array / 255.0 # normalize pixel values
 
-# Pass the preprocessed image to your model for prediction
-predictions = new_model.predict(img_array)
+    # Pass the preprocessed image to your model for prediction
+    predictions = new_model.predict(img_array)
 
-# Print the predicted outputs
-print("Preds----------------------------")
-print(predictions)
+    # Print the predicted outputs
+    # print("Preds----------------------------")
+    # print(predictions)
 
-predicted_class = np.argmax(predictions, axis=1)
-if predicted_class == 0:
-    print("Its a gun")
-else:
-    print("Not a gun")
+    predicted_class = np.argmax(predictions, axis=1)
+    if predicted_class == 0:
+        img_count['gun'] = img_count['gun'] +1
+    else:
+        img_count['no gun'] = img_count['no gun'] +1
+    i = i+1
+print(img_count)
